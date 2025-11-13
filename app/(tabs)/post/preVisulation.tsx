b2d00bcb2264224b514c1b919505ad8653e0fab2
Daniel Mattos <danielmattos.90228@gmail.com>
2025-11-13 12:44:12 -0300

    criando modal e page finish

--- app/(tabs)/post/preVisulation.tsx
+++ app/(tabs)/post/preVisulation.tsx
@@ -1,7 +1,10 @@
-import { router } from "expo-router";
+import { Link, router } from "expo-router";
+import { useState } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import Button from "../../../src/components/Button";
 import Carousel from "../../../src/components/Carousel";
+import AdConfirmationModalTailwind from "../../../src/components/Modals/AdModal";
 import TextArea from "../../../src/components/TextArea";
 
 const handleGoBack = () => {
@@ -9,8 +12,24 @@ const handleGoBack = () => {
 };
 
 export default function PreVisulation() {
+	const [isModalVisible, setIsModalVisible] = useState(false);
+	const NEXT_PAGE = "/(tabs)/post/history";
+
+	const handleNextPress = () => {
+		setIsModalVisible(true);
+	};
+
+	const handleModalContinue = () => {
+		// 1. Esconde o modal
+		setIsModalVisible(false);
+		// 2. Navega para a próxima página
+		router.push(NEXT_PAGE);
+	};
+	const handleModalClose = () => {
+		setIsModalVisible(false);
+	};
 	return (
-		<View>
+		<View className="flex-1">
 			<View className="p-8">
 				<TouchableOpacity className="mr-3" onPress={handleGoBack}>
 					<MaterialIcons name={"arrow-back"} size={25} color="#000" />
@@ -21,38 +40,65 @@ export default function PreVisulation() {
 				<View className="flex-row justify-between">
 					<View className="flex-row items-center justify-center gap-1">
 						<Text className="uppercase text-xl font-bold">rex</Text>
-            <MaterialIcons name={"edit"} size={25} color="#000" />
+						<MaterialIcons name={"edit"} size={25} color="#767676" />
 					</View>
-					<Text className="bg-red p-2 rounded-xl border text-white border-gray-500">Perdido</Text>
+					<Text className="bg-red p-2 rounded-xl border text-white border-gray-500">
+						Perdido
+					</Text>
 				</View>
 				<View>
-					<Text>Cachorro</Text>
+					<Text
+						className={`bg-blue-200 text-gray-500  flex justify-center items-center rounded-3xl w-20`}
+					>
+						Cachorro
+					</Text>
 				</View>
-				<View>
+				<View className="my-2">
+					<View className="flex-row items-center gap-1">
+						<Text className="text-base font-semibold">Descrição:</Text>
+						<MaterialIcons name={"edit"} size={25} color="#767676" />
+					</View>
 					<TextArea />
 				</View>
-				<View className="flex-row">
-					<MaterialIcons name={"location-on"} size={25} color="#000" />
-					<Text>Rua das Flores, Centro</Text>
-					<MaterialIcons name={"edit"} size={25} color="#000" />
+				<View className="gap-2 pb-8">
+					<View className="flex-row items-center gap-1">
+						<MaterialIcons name={"location-on"} size={25} color="#000" />
+						<Text className="text-base">Rua das Flores, Centro</Text>
+						<MaterialIcons name={"edit"} size={25} color="#767676" />
+					</View>
+					<View className="flex-row items-center gap-1">
+						<MaterialIcons name={"event"} size={25} color="#000" />
+						<Text className="text-base">Perdido(a) há</Text>
+						<Text className="text-red text-base">5 dias!</Text>
+						<MaterialIcons name={"notifications"} size={25} color="#EF5350" />
+						<MaterialIcons name={"edit"} size={25} color="#767676" />
+					</View>
+					<View className="flex-row items-center gap-1">
+						<MaterialIcons name={"person"} size={25} color="#000" />
+						<Text className="text-base">Reportado por Maria Silva</Text>
+						<MaterialIcons name={"edit"} size={25} color="#767676" />
+					</View>
+					<View className="flex-row items-center gap-1">
+						<MaterialIcons name={"call"} size={25} color="#000" />
+						<Text className="text-base">11 90000-0000</Text>
+						<MaterialIcons name={"edit"} size={25} color="#767676" />
+					</View>
 				</View>
-				<View className="flex-row">
-					<MaterialIcons name={"event"} size={25} color="#000" />
-					<Text>Perdido(a) há 5 dias! </Text>
-					<MaterialIcons name={"notifications"} size={25} color="#000" />
-					<MaterialIcons name={"edit"} size={25} color="#000" />
-				</View>
-				<View className="flex-row">
-					<MaterialIcons name={"person"} size={25} color="#000" />
-					<Text>Reportado por Maria Silva</Text>
-					<MaterialIcons name={"edit"} size={25} color="#000" />
-				</View>
-				<View className="flex-row">
-					<MaterialIcons name={"call"} size={25} color="#000" />
-					<Text>11 90000-0000</Text>
-					<MaterialIcons name={"edit"} size={25} color="#000" />
+				<View>
+					<Button text="Próximo" onPress={handleNextPress} />
+					<Link
+						className="text-center pt-4 text-red text-base"
+						href="/(tabs)/post"
+					>
+						Cancelar
+					</Link>
 				</View>
 			</View>
+			<AdConfirmationModalTailwind
+				isVisible={isModalVisible}
+				onContinue={handleModalContinue} // Navega apenas se Continuar for pressionado
+				onClose={handleModalClose} // Fecha se o usuário tentar sair (ex: botão Voltar no Android)
+			/>
 		</View>
 	);
 }
